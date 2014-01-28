603c254788cf65e793c2019eeddf52197b504868
kjlind <kjlind@cs.colostate.edu>
2014-01-27 23:27:26 -0700

    the /actual/ factory (forgot to add it...)

---
+++ 455/src/cs455/overlay/wireformats/MessageFactory.java
@@ -0,0 +1,42 @@
+package cs455.overlay.wireformats;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * MessageFactory consists of a static method to create a new Message instance
+ * of an appropriate type, based upon the contents of a byte array. This factory
+ * should primarily be used for unmarshalling bytes which have just been read
+ * from a stream.
+ * 
+ * @author Kira Lindburg
+ * @date Jan 27, 2014
+ */
+public class MessageFactory {
+    /**
+     * Creates a new Message of the appropriate type given the contents of the
+     * provided byte array. Assumes the first bytes in the array indicate the
+     * type of message, as according to the fields defined in Protocol.
+     * 
+     * @param marshalledBytes bytes containing the data of a message
+     * @return a Message of the appropriate type according to the type
+     * identifier at the beginning of the marshalled bytes
+     * @throws IOException if an I/O error occurs while trying to unmarshal the
+     * bytes
+     */
+    public static Message createMessage(byte[] marshalledBytes)
+            throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(bais));
+
+        int messageType = din.readInt();
+        switch (messageType) {
+            case Protocol.REGISTER_REQUEST:
+                return new RegisterRequest(marshalledBytes);
+            default:
+                throw new IOException("Unrecognized message type");
+        }
+    }
+}
---
+++ 455/src/cs455/tests/overlay/MessageFactoryTest.java
@@ -0,0 +1,59 @@
+package cs455.tests.overlay;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cs455.overlay.wireformats.Message;
+import cs455.overlay.wireformats.MessageFactory;
+import cs455.overlay.wireformats.Protocol;
+import cs455.overlay.wireformats.RegisterRequest;
+
+public class MessageFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testCreateRegisterRequest() {
+        // valid -- register request
+        String IPAddress = "denver.cs.colostate.edu";
+        int port = 5555;
+        String assignedID = "donuts";
+        try {
+            byte[] marshalledBytes = new RegisterRequest(IPAddress, port,
+                    assignedID).getBytes();
+            Message message = MessageFactory.createMessage(marshalledBytes);
+            assertEquals(message.getType(), Protocol.REGISTER_REQUEST);
+            RegisterRequest request = (RegisterRequest) message;
+            assertEquals(request.getIPAddress(), IPAddress);
+            assertEquals(request.getPort(), port);
+            assertEquals(request.getAssignedID(), assignedID);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Unexpected I/O error :( (see console output for details)");
+        }
+    }
+
+    @Test
+    public void testCreateInvalidType() {
+        // invalid -- unrecognized type
+        int badType = -1;
+        byte[] badBytes = ByteBuffer.allocate(4).putInt(badType).array();
+        try {
+            Message message = MessageFactory.createMessage(badBytes);
+            fail("No IOException thrown with an invalid message type identifier");
+            // this is purely so Eclipse quits whining about nothing being done
+            // with the message
+            System.out.println(message);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
